0fb8fbf133c155341cf53ce71690cce69f1ac4b8
Sadashiv H <sadamsrit@gmail.com>
2022-09-24 19:24:37 +0530

    Include docker add and cmd

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
     """ """
-    return HttpResponse("Welcome to Sample Application home page")
+    #return HttpResponse("Welcome to Sample Application home page 1.0")
+    text = """
+            <body style="background-color:green;">
+            <h1 align="center" style="text-color:green;">Welcome to Djano Application home page 3.0</h1>
+            </body>
+            """
+    return HttpResponse(text)
 
