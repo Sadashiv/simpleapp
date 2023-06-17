5f264a070b47efa9c1a9c435f0d4c700d09f4205
Sadashiv H <sadamsrit@gmail.com>
2023-06-17 21:07:38 +0530

    Added env variables

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM django
 
 EXPOSE 8000
-ADD https://files.pythonhosted.org/packages/a5/61/a867851fd5ab77277495a8709ddda0861b28163c4613b011bc00228cc724/requests-2.28.1.tar.gz /opt
+#ADD https://files.pythonhosted.org/packages/a5/61/a867851fd5ab77277495a8709ddda0861b28163c4613b011bc00228cc724/requests-2.28.1.tar.gz /opt
 COPY . /opt
 WORKDIR /opt
 RUN pip install -r requirements.txt
 ENTRYPOINT  ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
-CMD ["echo", "Welocme to python"]
+#CMD ["Welocme to python"]
--- app/views.py
+++ app/views.py
@@ -6,11 +6,14 @@ from django.shortcuts import redirect
 # Create your views here.
 def index(request):
     """ """
+        """ """
+    get_color = os.environ.get('APP_COLOR')
     #return HttpResponse("Welcome to Sample Application home page 1.0")
     text = """
-            <body style="background-color:green;">
-            <h1 align="center" style="text-color:green;">Welcome to Djano Application home page 3.0</h1>
+            <body style="background-color:%s;">
+            <h1 align="center" style="text-color:%s;">Welcome to Djano Application with APP_COLOR</h1>
             </body>
-            """
+            """ %(get_color,get_color)
+
     return HttpResponse(text)
 
